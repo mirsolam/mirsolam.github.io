d8bae3636e9f23af369f0cf61cbc4923c42e14c3
mirsolam <167993404+mirsolam@users.noreply.github.com>
2025-08-22 16:35:40 +0300

    Improve Intersection observer of the nav panel

--- src/app/(functions)/_intersection_observer.tsx
+++ src/app/(functions)/_intersection_observer.tsx
@@ -1,5 +1,10 @@
-import { Dispatch, RefObject, SetStateAction } from "react"
-import { SECONDARY_BACKGROUND, PRIMARY_BACKGROUND, getNavTexts, getSections } from "./_global_functions";
+import { Dispatch, RefObject, SetStateAction } from "react";
+import {
+    SECONDARY_BACKGROUND,
+    PRIMARY_BACKGROUND,
+    getNavTexts,
+    getSections,
+} from "./_global_functions";
 
 function _buildThresholdList() {
     const thresholds = [];
@@ -14,77 +19,106 @@ function _buildThresholdList() {
     return thresholds;
 }
 
-export default function initializeObserver(observerRef: RefObject<{main: IntersectionObserver|null, prev: { id: string, theme: string }}>, setActive: Dispatch<SetStateAction<string>>) {
+export default function initializeObserver(
+    observerRef: RefObject<{
+        main: IntersectionObserver | null;
+        prev: { id: string; theme: string };
+    }>,
+    setActive: Dispatch<SetStateAction<string>>
+) {
     const options = {
         root: null,
         rootMargin: "0px",
         threshold: _buildThresholdList(),
     };
 
-    const nav = document.getElementById("nav")
-    const mobileMenuButtonSecondary = document.getElementById("mobile-menu-button-secondary")
-    const mobileMenuButtonPrimary = document.getElementById("mobile-menu-button-primary")
-    const mobileMenu = document.getElementById("mobile-menu")
-    const navTexts = getNavTexts()
-    const sections = getSections()
-    const isNavHidden = window.getComputedStyle(nav!, null).display === "none"
+    const nav = document.getElementById("nav");
+    const mobileMenuButtonSecondary = document.getElementById(
+        "mobile-menu-button-secondary"
+    );
+    const mobileMenuButtonPrimary = document.getElementById(
+        "mobile-menu-button-primary"
+    );
+    const mobileMenu = document.getElementById("mobile-menu");
+    const navTexts = getNavTexts();
+    const sections = getSections();
+    const isNavHidden = window.getComputedStyle(nav!, null).display === "none";
 
-    if (!nav || !sections || !navTexts) return
+    if (!nav || !sections || !navTexts) return;
 
     function intersectionHandler(entries: Array<IntersectionObserverEntry>) {
         entries.forEach((entry) => {
-            const theme = entry.target.classList[0]
-            const id = entry.target.id
-            const primaryColor = theme === "primary-theme" ? PRIMARY_BACKGROUND.rgb : SECONDARY_BACKGROUND.rgb
-            const secondaryColor = theme === "primary-theme" ? SECONDARY_BACKGROUND.rgb : PRIMARY_BACKGROUND.rgb
-
-            navTexts.map(el => {
-                const item = el as HTMLElement
-                if (entry.boundingClientRect.top <= item.getBoundingClientRect().bottom)
-                    if (item.style.color !== secondaryColor && !isNavHidden)
-                        item.style.color = secondaryColor
-                    else if (isNavHidden)
-                        item.style.color = primaryColor
-            })
+            const theme = entry.target.classList[0];
+            const id = entry.target.id;
+            const primaryColor =
+                theme === "primary-theme"
+                    ? PRIMARY_BACKGROUND.rgb
+                    : SECONDARY_BACKGROUND.rgb;
+            const secondaryColor =
+                theme === "primary-theme"
+                    ? SECONDARY_BACKGROUND.rgb
+                    : PRIMARY_BACKGROUND.rgb;
 
             if (entry.boundingClientRect.top <= nav!.getBoundingClientRect().bottom) {
-                if (theme === observerRef.current.prev.theme && nav!.style.borderImage.includes(`${secondaryColor} 100%`)) {
-                    if (entry.intersectionRatio > 0.94) {
-                        setActive(id.split("_")[1])
+                if (
+                    theme === observerRef.current.prev.theme &&
+                    nav!.style.borderImage.includes(`${secondaryColor} 100%`)
+                ) {
+                    if (entry.intersectionRatio > 0.84) {
+                        setActive(id.split("_")[1]);
                     }
-                    return
+                    return;
                 }
-                nav!.style.borderImage = `linear-gradient(0deg, ${secondaryColor} ${entry.intersectionRatio * 100}%, ${primaryColor} 0)`
-                nav!.style.borderImageSlice = "1"
-
-                mobileMenu!.style.backgroundColor = secondaryColor
+                const gradient = `linear-gradient(0deg, ${secondaryColor} ${entry.intersectionRatio * 100}%, ${primaryColor} 0)`
+                if (nav!.style.borderImage !== gradient) {
+                    nav!.style.borderImage = gradient;
+                    nav!.style.borderImageSlice = "1";
+                }
+                mobileMenu!.style.backgroundColor = secondaryColor;
 
                 if (isNavHidden) {
                     if (theme === "primary-theme") {
-                        mobileMenuButtonPrimary!.style.display = "none"
-                        mobileMenuButtonSecondary!.style.display = "block"
-                    }
-                    else {
-                        mobileMenuButtonPrimary!.style.display = "block"
-                        mobileMenuButtonSecondary!.style.display = "none"
+                        mobileMenuButtonPrimary!.style.display = "none";
+                        mobileMenuButtonSecondary!.style.display = "block";
+                    } else {
+                        mobileMenuButtonPrimary!.style.display = "block";
+                        mobileMenuButtonSecondary!.style.display = "none";
                     }
                 }
 
-
-                if (entry.intersectionRatio > 0.94) {
-                    nav!.style.borderImage = `linear-gradient(0deg, ${secondaryColor} 100%, ${primaryColor} 0)`
-                    nav!.style.borderImageSlice = "1"
-                    observerRef.current.prev.theme = theme
-                    setActive(id.split("_")[1])
+                if (entry.intersectionRatio > 0.9) {
+                    nav!.style.borderImage = `linear-gradient(0deg, ${secondaryColor} 100%, ${primaryColor} 0)`;
+                    nav!.style.borderImageSlice = "1";
+                    observerRef.current.prev.theme = theme;
+                    setActive(id.split("_")[1]);
                 }
             }
+
+            navTexts.map((el) => {
+                const item = el as HTMLElement;
+                if (!isNavHidden) {
+                    if (entry.boundingClientRect.top <= item.getBoundingClientRect().bottom)
+                        if (item.style.color !== secondaryColor)
+                            item.style.color = secondaryColor;
+                } else {
+                    if (mobileMenu!.style.backgroundColor === secondaryColor) {
+                        item.style.color = primaryColor;
+
+                    }
+                    else {
+                        item.style.color = secondaryColor;
+                    }
+                }
+            });
         });
     }
 
+    observerRef.current.main = new IntersectionObserver(
+        intersectionHandler,
+        options
+    );
 
-    observerRef.current.main = new IntersectionObserver(intersectionHandler, options);
-
-    sections.map(item => {
-        observerRef.current.main!.observe(item)
-    })
+    sections.map((item) => {
+        observerRef.current.main!.observe(item);
+    });
 }
